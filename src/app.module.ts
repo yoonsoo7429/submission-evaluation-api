38d93c6051d02700b276a7ead631ca198315adee
YOONSOO7429 <125416958+YOONSOO7429@users.noreply.github.com>
2025-04-25 13:00:24 +0900

    build: 라이브러리 설치 및 DB, AZURE 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,48 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as Joi from 'joi';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { envVariables } from './common/const/env.const';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        // DB
+        DB_HOST: Joi.string().required(),
+        DB_DATABASE: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        // AZURE_BLOB
+        AZURE_CONNECTION_STRING: Joi.string().required(),
+        AZURE_CONTAINER: Joi.string().required(),
+        // AZURE_OPENAI
+        AZURE_OPENAI_ENDPOINT: Joi.string().uri().required(),
+        AZURE_OPENAI_KEY: Joi.string().required(),
+        AZURE_OPENAI_DEPLOYMENT: Joi.string().required(),
+        AZURE_OPENAI_API_VERSION: Joi.string().required(),
+      }),
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>(envVariables.dbHost),
+        port: configService.get<number>(envVariables.dbPort),
+        username: configService.get<string>(envVariables.dbUsername),
+        password: configService.get<string>(envVariables.dbPassword),
+        database: configService.get<string>(envVariables.dbDatabase),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+        // synchronize: false,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/common/const/env.const.ts
@@ -0,0 +1,30 @@
+// DB
+const dbHost = 'DB_HOST';
+const dbDatabase = 'DB_DATABASE';
+const dbPort = 'DB_PORT';
+const dbUsername = 'DB_USERNAME';
+const dbPassword = 'DB_PASSWORD';
+
+// Azure Blob
+const azureConnectionString = 'AZURE_CONNECTION_STRING';
+const azureContainer = 'AZURE_CONTAINER';
+
+// Azure OpenAI
+const azureOpenAIEndpoint = 'AZURE_OPENAI_ENDPOINT';
+const azureOpenAIKey = 'AZURE_OPENAI_KEY';
+const azureOpenAIDeployment = 'AZURE_OPENAI_DEPLOYMENT';
+const azureOpenAIApiVersion = 'AZURE_OPENAI_API_VERSION';
+
+export const envVariables = {
+  dbHost,
+  dbDatabase,
+  dbPassword,
+  dbPort,
+  dbUsername,
+  azureConnectionString,
+  azureContainer,
+  azureOpenAIEndpoint,
+  azureOpenAIKey,
+  azureOpenAIDeployment,
+  azureOpenAIApiVersion,
+};
